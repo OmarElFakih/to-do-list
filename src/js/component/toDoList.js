1ecdbd7538d32a663a6b6e02b795566cf4e69fe3
OmarElFakih <omarelfakih97@gmail.com>
2020-08-03 16:26:24 +0000

    estilos creados

---
+++ src/js/component/toDoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function ToDoList(Props) {
+	return (
+		<div className="paper">
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">Cras justo odio</li>
+				<li className="list-group-item">Dapibus ac facilisis in</li>
+				<li className="list-group-item">Morbi leo risus</li>
+				<footer className="list-footer">footer</footer>
+			</ul>
+		</div>
+	);
+}
